eaf23a6d6d187c090e3af6c7b1d4007bb580b21e
gscoon <gscoon@gmail.com>
2015-08-18 20:57:52 -0400

    ext

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,6 +4,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.browserAction.setBadgeBackgroundColor({color: '#3cb73e'});
+chrome.browserAction.setBadgeText({text: 'GS'});
 
 console.log('\'Allo \'Allo! Event Page for Browser Action');
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,34 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+ window.onload = function(){
+     lev.start();
+ }
+
+ var lev = new function(){
+
+     this.start = function(){
+        pageHandler();
+     }
+
+     function pageHandler() {
+         switch(window.location.hostname){
+             case 'www.facebook.com':
+             case 'facebook.com':
+                handleFacebook();
+                break;
+            case 'www.instagram.com':
+            case 'instagram.com':
+                handleInstagram();
+                break;
+         }
+
+     }
+
+     function handleFacebook(){
+         console.log('handleFacebook');
+     }
+
+     function handleInstagram(){
+         console.log('handleInstagram');
+     }
+ }
